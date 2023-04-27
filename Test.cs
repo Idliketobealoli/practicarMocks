a3049905bc4ef56a35c6175d4212f57874879c1f
Daniel Rodriguez Muñoz <daniel.rodriguez@gosupplyservices.com>
2023-04-27 11:27:24 +0200

    test

---
+++ Test.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace practicarMocks
+{
+    [TestClass]
+    public class Test
+    {
+        public Mock<ClaseDos> C2;
+        public ClaseUno C1;
+
+        [TestInitialize]
+        public void Init()
+        {
+            C2 = new Mock<ClaseDos>();
+            C1 = new(C2.Object);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            C2.Setup(x => x.DameString()).Returns("owo");
+
+            var res = C1.HazCosas(1);
+
+            Assert.IsNotNull(res);
+            Assert.AreEqual("Numero 1 : owo", res);
+        }
+    }
+}
